afa73b43a4cf3acc9f3ee8ba442ff14a1adaacbe
Kkobari <barca.kovalska@gmail.com>
2024-10-03 18:32:29 +0200

    Init

---
+++ function/Sphere.py
@@ -0,0 +1,7 @@
+# source: https://www.sfu.ca/~ssurjano/spheref.html
+
+class Sphere:
+    def __init__(self):
+        self.bounds = None
+        self.bounds.lower = -5.12
+        self.bounds.upper = 5.12
